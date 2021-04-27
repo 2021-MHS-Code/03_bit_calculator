d71f1f4387a39b26893ffedc8127ec530a500774
jgottschalkmhs <49209767+jgottschalkmhs@users.noreply.github.com>
2021-04-27 21:30:55 +1200

    complete calculator

--- 00_bit_calc.py
+++ 00_bit_calc.py
@@ -16,6 +16,20 @@ def statement_generator(text, decoration):
     return ""
     
 
+# displays instructions / information
+def instructions():
+    
+    statement_generator("Instructions /  Information", "=")
+    print()
+    print("Please choose a data type (image / text / integer)")
+    print()
+    print("This program assumes that images are being represented in 24 bit colour (ie: 24 bits per pixel).  For text, we assume that ascii encoding is being used (8 bits per character).")
+    print()
+    print("Complete as many calculations as necessary, pressing <enter> at the end of each calculation or any key to quit.")
+    print()
+    return ""
+
+
 # checks user choice is 'integer', 'text' or 'image'
 def user_choice():
     
@@ -54,12 +68,110 @@ def user_choice():
             print()
         
 
+# checks input is a number more than a given value
+def num_check(question, low):
+    valid = False
+    while not valid:
+        
+        error = "Please enter an integer that is more than (or equal to) {}".format(low)
+        
+        try:
+        
+            # ask user to enter a number
+            response = int(input(question))
+            
+            # checks number is more than zero
+            if response >= low:
+                return response
+            
+            # outputs error if input is invalid
+            else:
+                print(error)
+                print()
+                
+        except ValueError:
+            print(error)    
+    
+
+# calculates the # of bits for text (# of characters x 8)
+def text_bits():
+    
+    print()
+    # ask user for a string...
+    var_text = input("Enter some text: ")
+    
+    # calculate # of bits (length of string x 8)
+    var_length = len(var_text)
+    num_bits = 8 * var_length
+      
+    # output answer with working
+    print()
+    print("\'{}\' has {} characters ...".format(var_text, var_length))
+    print("# of bits is {} x 8...".format(var_length))
+    print("We need {} bits to represent {}".format(num_bits, var_text))
+    print()
+    
+    return ""
+
+
+# finds # of bits for 24 bit colour
+def image_bits():
+      
+    # get width and height...
+    image_width = num_check("Image width? ", 1)
+    image_height = num_check("Image height? ", 1)
+    
+    # calculate # of pixels
+    num_pixels = image_width * image_height
+    
+    # calculate # bits (24 x num pixels)
+    num_bits = num_pixels * 24
+      
+    # output answer with working
+    print()
+    print("# of pixels = {} x {} = {}".format(image_height, 
+                                              image_width, num_pixels))
+    print("# bits = {} x 24 = {}".format(num_pixels, num_bits))
+    print()
+
+    return ""
+
+
+# converts decimal to binary and states how 
+# many bits are needed to represent the original integer
+def int_bits():
+    
+    # get integer (must be >= 0)
+    var_integer = num_check("Please enter an integer: ", 0)
+    
+    # source for code below is 
+    # https://stackoverflow.com/questions/699866/python-int-to-binary-string
+    
+    var_binary = "{0:b}".format(var_integer)
+    
+    # calculate # of bits (length of string above)
+    num_bits = len(var_binary)
+    
+     # output answer with working
+    print()
+    print("{} in binary is {}".format(var_integer, var_binary))
+    print("# of bits is {}".format(num_bits))
+    print()
+    
+    return ""
+    
+
+
 # Main Routine goes here
 
 # Heading
 statement_generator("Bit Calcultor for Integers, Text & Images", "-")
 
 # Display instructions if user has not used the program before
+first_time = input("Press <enter> to see the instructions or any key to continue")
+
+if first_time == "":
+    instructions()
 
 # Loop to allow multiple calculations per session
 keep_going = ""
@@ -70,11 +182,18 @@ while keep_going == "":
     print("You chose", data_type)
     
     # For integers, ask for integer 
-    # (must be an integer more than / equal to 0)
-    
+    if data_type =="integer":
+        int_bits()
+
     # For images, ask for width and height 
     # (must be integers more than / equal to 1)
-    
+    elif data_type == "image":
+        image_bits()
+      
     # For text, ask for a string
+    else:
+        text_bits()
 
-
+    print()
+    keep_going = input("Press <enter> to continue or any key to quit ")
+    print()
--- 05_image_bits.py
+++ 05_image_bits.py
@@ -1,4 +1,5 @@
 # checks input is a number more than a given value
+# checks input is a number more than a given value
 def num_check(question, low):
     valid = False
     while not valid:
@@ -23,17 +24,27 @@ def num_check(question, low):
             print(error)    
     
 
-# Main Routine goes here
-
-keep_going = ""
-while keep_going == "":
-    print()
-    # ask user for an integer (must be more than / equal to 0)
-    var_integer = num_check("Enter an integer: ", 0)
-    print()
-    
-    # ask for width & height of an image 
-    # (must be more than / equal to 1)
+# finds # of bits for 24 bit colour
+def image_bits():
+      
+    # get width and height...
     image_width = num_check("Image width? ", 1)
-    print()
     image_height = num_check("Image height? ", 1)
+    
+    # calculate # of pixels
+    num_pixels = image_width * image_height
+    
+    # calculate # bits (24 x num pixels)
+    num_bits = num_pixels * 24
+      
+    # output answer with working
+    print()
+    print("# of pixels = {} x {} = {}".format(image_height, 
+                                              image_width, num_pixels))
+    print("# bits = {} x 24 = {}".format(num_pixels, num_bits))
+    print()
+
+    return ""
+
+# Main routine goes here
+image_bits()
